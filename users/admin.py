c3278d4dc0bcb3a6fb0d860435931a958edd737c
Andrey <moregun@list.ru>
2023-10-14 23:01:57 +0300

    migrate commit

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
-# Register your models here.
+from users.models import User
+
+
+@admin.register(User)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('name', 'surname', 'email',)
+    list_filter = ('name',)
+    search_fields = ('name', 'surname', 'email')
